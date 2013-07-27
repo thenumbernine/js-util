446b785e4d0e6617700fb11a2437b7fd10db8dd5
Chris Moore <christopher.e.moore@gmail.com>
2013-07-26 22:37:29 -0600

    fix for black hole skymap unit quad

--- gl-util-unitquad.js
+++ gl-util-unitquad.js
@@ -16,14 +16,21 @@ GL.oninit.push(function(gl) {
 		1,1
 	]);
 
+	this.unitQuadVertexBuffer = new GL.ArrayBuffer({
+		dim : 2,
+		data : this.unitQuadVertexes,
+		keep : true
+	});
+
+	this.unitQuadGeom = new GL.Geometry({
+		mode : gl.TRIANGLE_STRIP,
+		vertexes : this.unitQuadVertexBuffer
+	});
+
 	this.unitQuad = new GL.SceneObject({
 		mode : gl.TRIANGLE_STRIP,
 		attrs : {
-			vertex : new GL.ArrayBuffer({
-				dim : 2,
-				data : this.unitQuadVertexes,
-				keep : true
-			})
+			vertex : this.unitQuadVertexBuffer
 		},
 		parent : null,
 		static : true
